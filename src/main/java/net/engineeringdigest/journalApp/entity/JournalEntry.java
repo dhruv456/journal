994419a6f3fa2c614f7fc4c687f11bb82d18658d
dvj-lct <dhuvjain1977@gmail.com>
2025-04-03 01:49:54 +0530

    change collection name of JournalEntry

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -8,6 +8,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
+
+@Document(collection = "journalEntries")
 @Getter
 @Setter
 public class JournalEntry {
